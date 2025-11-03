638773b2cd96500466e57fae10c892a22972dfaa
jinho-yoo-jack <jhy7342@gmail.com>
2025-11-03 16:04:46 +0900

    INIT

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
+
+	err := r.Run(":8080")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
